51415ee53147acb1af00834489273c3aa859ce48
ndrplz <ndrplz@gmail.com>
2017-12-08 15:09:23 +0100

    Add download link to setup

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     packages=['google_drive_downloader'],
     url='https://github.com/ndrplz/google-drive-downloader',
+    download_url='https://github.com/ndrplz/google-drive-downloader/archive/0.2.tar.gz',
     license='MIT',
     author='Davide Abati and Andrea Palazzi',
     author_email='ndrplz@gmail.com',
