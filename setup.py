332759dad1c206db8e7fb1c2c27f6d4f49bb9f74
ndrplz <ndrplz@gmail.com>
2017-12-08 14:33:40 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='googledrivedownloader',
+    version='0.1',
+    packages=['google_drive_downloader'],
+    url='https://github.com/ndrplz/google-drive-downloader',
+    license='MIT',
+    author='Davide Abati and Andrea Palazzi',
+    author_email='ndrplz@gmail.com',
+    description='Minimal class to download shared files from Google Drive.'
+)
